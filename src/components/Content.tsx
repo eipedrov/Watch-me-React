aaccddbe8c08834dd6becee17ea50d6586e90987
Pedro Vinicius <silvapv7@gmail.com>
2022-03-26 14:55:05 -0400

    Watch me Componentizado :D

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,55 @@
-export function Content() {
-  // Complete aqui
+//Componente filho
+
+//imports
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+//Tipagens
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number; 
+  selectedGenre: GenreResponseProps;
+}
+
+//funcoes
+export function Content({selectedGenreId, selectedGenre}:ContentProps) {//desestruturacao das funcoes
+  const [movies, setMovies] = useState<MovieProps[]>([]); //retorna os filmes e suas informacoes
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]); //retorna os filmes por genero
+
+  return( //funcao content e suas funcionalidades
+  <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+    <main>
+      <div className="movies-list"> 
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+//Componente filho
+
+//Imports
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+//Tipagens
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+//funcoes
+export function SideBar({handleClickButton,selectedGenreId } : SideBarProps) { //desestruturacao das funcoes
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => { //separa os filmes por genero
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+  
+  return( //estruturacao da sidebar
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id} //filtra quais filmes aparecerao pelo genero(funcao filho)
+            />
+          ))}
+        </div>
+      </nav>
+  )
 }
